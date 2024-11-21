256906838d0fce29d197fb69a0baafd47da0d976
Jochem Kuipers <jochem@kuipers.cc>
2024-11-21 10:10:28 +0100

    check precommit checks

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         self.settingsButton = self.findChild(QtWidgets.QPushButton, "settingsButton")
         self.settingsButton.clicked.connect(self.open_settings)
-        
+
         self.settingswindow = None
 
         self.show()
@@ -19,17 +19,17 @@ class MainWindow(QtWidgets.QMainWindow):
         if not self.settingswindow:
             self.settingswindow = SettingsWindow()
         self.settingswindow.show()
-        
+
 class SettingsWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("components/settings.ui", self) # type: ignore
-        
+
         self.backButton = self.findChild(QtWidgets.QPushButton, "backButton")
         self.backButton.clicked.connect(self.close)
 
         self.show()
-        
+
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
